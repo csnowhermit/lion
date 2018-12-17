a8ad63328aa0f3d575a5835d3e246282f395ffa3
RxtBD <920164255@qq.com>
2018-12-17 16:30:59 +0800

    add code comment

--- lion-boot/src/main/java/com/lion/vip/bootstrap/Main.java
+++ lion-boot/src/main/java/com/lion/vip/bootstrap/Main.java
@@ -15,6 +15,15 @@ public class Main {
         addHook(serverLauncher);
     }
 
+    /**
+     * 注意：
+     * 1.不要在ShutdownHook Thread里调用System.exit()方法，否则会造成死循环；
+     * 2.如果有非守护线程，只有所有的非守护线程都结束了才会执行hook；
+     * 3.Thread默认为非守护线程；
+     * 4.注意线程抛出的异常，如果没有被捕获到，都会跑到Thread.dispatchUncauhtException
+     *
+     * @param serverLauncher
+     */
     private static void addHook(ServerLauncher serverLauncher) {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             try {
